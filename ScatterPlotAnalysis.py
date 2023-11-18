25d799d25d1254b153d5aa34a6dee4f3ebd2470e
Christoph Gödecke <cgodecke@student.42wolfsburg.de>
2023-11-18 02:46:29 +0100

    added refresh plot button

--- ScatterPlotAnalysis.py
+++ ScatterPlotAnalysis.py
@@ -9,6 +9,8 @@ class ScatterPlotAnalysis(BaseAnalysis):
 		super().__init__()
 		self.main_app = main_app
 		self.plot_title = tk.StringVar(value = "Scatter Plot")
+		self.scatter_last_window = None
+		self.scatter_last_canvas = None
 
 	def show_scatter_plot(self):
 		selected_columns = self.main_app.get_selected_columns()
@@ -18,17 +20,37 @@ class ScatterPlotAnalysis(BaseAnalysis):
 			plt.xlabel(selected_columns[0])
 			plt.ylabel(selected_columns[1])
 			plt.title(self.plot_title.get())
-			self.display_plot(fig)
+			self.scatter_last_window, self.scatter_last_canvas = self.display_plot(fig, self.scatter_last_window, self.scatter_last_canvas)
 		else:
 			messagebox.showinfo("Information", "Select two Columns")
 
-	def display_plot(self, fig):
-		new_window = tk.Toplevel(self.main_app.tk_root)
-		new_window.title(self.plot_title.get())
+	def refresh_scatter_plot(self):
+		selected_columns = self.main_app.get_selected_columns()
+		if len(selected_columns) == 2:
+			fig = plt.figure(figsize=(8, 6))
+			plt.scatter(self.main_app.df[selected_columns[0]], self.main_app.df[selected_columns[1]])
+			plt.xlabel(selected_columns[0])
+			plt.ylabel(selected_columns[1])
+			plt.title(self.plot_title.get())
+			self.scatter_last_canvas = self.display_refresh_plot(fig, self.scatter_last_canvas)
+		else:
+			messagebox.showinfo("Information", "Select two Columns")
 
-		canvas = FigureCanvasTkAgg(fig, master=new_window)
-		canvas.draw()
-		canvas.get_tk_widget().pack()
+	def display_refresh_plot(self, fig, scatter_last_canvas):
+		#canvas = FigureCanvasTkAgg(fig, master=self.last_window)
+		scatter_last_canvas.figure = fig
+		scatter_last_canvas.draw()
+		scatter_last_canvas.get_tk_widget().pack()
+		return scatter_last_canvas
+
+	def display_plot(self, fig, scatter_last_window, scatter_last_canvas):
+		scatter_last_window = tk.Toplevel(self.main_app.tk_root)
+		scatter_last_window.title(self.plot_title.get())
+
+		scatter_last_canvas = FigureCanvasTkAgg(fig, master=scatter_last_window)
+		scatter_last_canvas.draw()
+		scatter_last_canvas.get_tk_widget().pack()
+		return scatter_last_window, scatter_last_canvas
 
    
 	def init_ui(self, parent_frame):
@@ -42,8 +64,11 @@ class ScatterPlotAnalysis(BaseAnalysis):
 
 		# Button zum Anzeigen des Scatter Plots
 		show_plot_button = tk.Button(button_frame, text="Show Plot", command=self.show_scatter_plot)
+		show_plot_button.pack(side = tk.LEFT, padx=5, pady=5)
+  
+		# Button zum Erneuern des Scatter Plots
+		show_plot_button = tk.Button(button_frame, text="Refresh Plot", command=self.refresh_scatter_plot)
 		show_plot_button.pack(padx=5, pady=5)
-     
 
 		# Frame für den Titel und das Eingabefeld
 		title_frame = tk.Frame(parent_frame)
--- gui_components.py
+++ gui_components.py
@@ -26,12 +26,30 @@ class DataAnalysisApp:
 		open_button = tk.Button(input_data_frame, text="Open CSV file", command=lambda: open_file(self))
 		open_button.pack(side=tk.LEFT, padx=5, pady=5)
 
-		# CSV columns frame
+		# Closing main window
+		tk_root.protocol("WM_DELETE_WINDOW", self.on_close)
+
+		# CSV columns frame with scrollable area
 		self.columns_frame = tk.Frame(tk_root)
 		self.columns_frame.pack()
 
-		# Closing main window
-		tk_root.protocol("WM_DELETE_WINDOW", self.on_close)
+		# Scrollable area for columns
+		self.columns_canvas = tk.Canvas(self.columns_frame)
+		self.columns_scrollbar = tk.Scrollbar(self.columns_frame, orient="vertical", command=self.columns_canvas.yview)
+		self.scrollable_frame = tk.Frame(self.columns_canvas)
+
+		self.scrollable_frame.bind(
+			"<Configure>",
+			lambda e: self.columns_canvas.configure(
+				scrollregion=self.columns_canvas.bbox("all")
+			)
+		)
+
+		self.columns_canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
+		self.columns_canvas.configure(yscrollcommand=self.columns_scrollbar.set)
+
+		self.columns_canvas.pack(side="left", fill="both", expand=True)
+		self.columns_scrollbar.pack(side="right", fill="y")
 
 
 
@@ -73,12 +91,12 @@ class DataAnalysisApp:
 			self.line_chart_analysis.init_ui(self.line_chart_tab)
 
 	def update_column_checklist(self):
-		for widget in self.columns_frame.winfo_children():
+		for widget in self.scrollable_frame.winfo_children():
 			widget.destroy()
 
 		for column in self.df.columns:
 			var = tk.BooleanVar()
-			checkbox = tk.Checkbutton(self.columns_frame, text=column, variable=var)
+			checkbox = tk.Checkbutton(self.scrollable_frame, text=column, variable=var)
 			checkbox.pack(anchor='w')
 			self.columns_to_analyze[column] = {"var": var, "text": column}
 			
