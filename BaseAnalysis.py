55d6f61d53e843c336749f0effb0552ce8bb8a70
Christoph Gödecke <cgodecke@student.42wolfsburg.de>
2023-11-20 22:20:57 +0100

    moved function display_plot and display_refresh_plot and save_plot into BaseAnalysis class

--- BaseAnalysis.py
+++ BaseAnalysis.py
@@ -1,7 +1,40 @@
+from tkinter import filedialog
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import tkinter as tk
 
 class BaseAnalysis:
 	def __init__(self):
 		pass
 
-	def perform_analysis(self):
-		raise NotImplementedError("Diese Methode muss in der abgeleiteten Klasse implementiert werden.")
+	def display_plot(self, fig, scatter_last_window, scatter_last_canvas):
+		scatter_last_window = tk.Toplevel(self.main_app.tk_root)
+		scatter_last_window.title(self.plot_title.get())
+		save_button = tk.Button(scatter_last_window, text="Save Plot", command=lambda: self.save_plot(fig))
+		save_button.pack()
+
+		scatter_last_canvas = FigureCanvasTkAgg(fig, master=scatter_last_window)
+		scatter_last_canvas.draw()
+		scatter_last_canvas.get_tk_widget().pack()
+		return scatter_last_window, scatter_last_canvas
+
+	def display_refresh_plot(self, fig, scatter_last_canvas):
+		#canvas = FigureCanvasTkAgg(fig, master=self.last_window)
+		scatter_last_canvas.get_tk_widget().destroy()
+		scatter_last_canvas = FigureCanvasTkAgg(fig, master=self.scatter_last_window)
+		scatter_last_canvas.draw()
+		width = fig.get_figwidth() * fig.dpi
+		height = fig.get_figheight() * fig.dpi
+		self.scatter_last_window.geometry(f"{int(width)}x{int(height)}")
+		scatter_last_canvas.get_tk_widget().pack()
+		return scatter_last_canvas
+
+	def save_plot(self, fig):
+		plot_title = fig._suptitle.get_text() if fig._suptitle else "MyPlot"
+		filepath = filedialog.asksaveasfilename(
+			defaultextension='',
+			filetypes=[("PNG files", "*.png"), ("All Files", "*.*")],
+			title="Save plot",
+			initialfile= plot_title
+		)
+		if filepath:
+			fig.savefig(filepath)
